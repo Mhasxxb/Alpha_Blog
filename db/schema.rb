92d42d5f838dc3ff51b940130f0b1b397e85fc27
Muhammad Haseeb <muhmmedhaseeb2003@gmail.com>
2026-01-12 17:11:03 +0500

    Added validations on the Article model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20260112085413) do
+ActiveRecord::Schema.define(version: 20260112100907) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
