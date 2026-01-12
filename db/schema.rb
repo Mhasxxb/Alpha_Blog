7b99c93f5dbf0830852d84e3e9535399222e3d1b
Muhammad Haseeb <muhmmedhaseeb2003@gmail.com>
2026-01-12 14:39:30 +0500

    dropped the old db and migrations made a new table articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120234529) do
-
-  create_table "article_categories", force: :cascade do |t|
-    t.integer "article_id"
-    t.integer "category_id"
-  end
+ActiveRecord::Schema.define(version: 20260112085413) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.boolean "admin", default: false
   end
 
 end
