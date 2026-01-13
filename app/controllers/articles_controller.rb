518f106744be4618b410d5f6d554503722f08f05
Muhammad Haseeb <muhmmedhaseeb2003@gmail.com>
2026-01-13 13:16:39 +0500

    Added show action within ArticlesController.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
 
   def show
+    
+    #article = Article.find(params[:id]) ###over here it is just a standard variable which is not passed in to your views
+    #for passing it you will be needing an instance variable instead of a regular variable
+    # byebug
+    @article = Article.find(params[:id])
+    # @article = Article.find(params["id"])  ### both will work in the same way
+
+
   end
 
 end
