9aa2be56e953ced033234ce06d187d7e292fcaaa
Muhammad Haseeb <muhmmedhaseeb2003@gmail.com>
2026-01-14 13:23:27 +0500

    Added functionality within the edit and update action of the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,20 +33,55 @@ class ArticlesController < ApplicationController
 
       redirect_to article_path(@article)
     else
-      render "new"
+
+      # render "new" works the same way
+      render :new
+
     end
   end
 
   def edit
+    #@article = Article.new                 ## this will let you pass through the GET request easily
+
     
-    @article = Article.new()
-  
+
+    ##@article = Article.new
+    ##@article.title = "fghjk"
+    ##@article.description = "dfghjkldfghjkvbnk"       ##this will add these values on the input fields as the form is 
+                                                       ##capturing the instace variable
+
+
+    @article = Article.find(params[:id])    ## This will get you the fields auto filled hence allowing you to 
+                                            ## take full advantage of model: @article as it will access the url 
+                                            ## for you as well as it will know which mode to access via the instance variable it got
   end
 
   def update 
-  
+    
+    @article = Article.find(params["id"])
 
-  
+    check = @article.title == params[:article][:title] and @article.description == params[:article][:description]
+
+    if check
+
+      flash[:alert] = "No changes were made."
+      redirect_to @article
+
+    else
+
+      check = @article.update(params.require(:article).permit(:title, :description))
+
+      if check
+
+        flash[:notice] = "Your article was updated successfully. "
+        redirect_to @article
+
+      else
+
+        render :edit
+        
+      end
+    end
   end
 
 end
