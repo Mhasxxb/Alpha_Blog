0304a59b80f87ada60c32e0145ab60a4fec6bc54
Muhammad Haseeb <muhmmedhaseeb2003@gmail.com>
2026-01-22 14:30:15 +0500

    done with edit action and edit form with flash messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,15 @@ class UsersController < ApplicationController
 before_action :get_user, only: [:edit, :update, :show ]
 def new
   @user = User.new
+  @text = "Sign up for MyBlog"
+
 end
 
 def create
   @user = User.new(get_user_params)
   if @user.save
 
-    flash[:notice] = "User created successfully"
+    flash[:notice] = "User created successfully! Welcome, #{@user.username}"
 
     redirect_to root_path
   else 
@@ -17,9 +19,24 @@ def create
 end
 
 def edit 
+  @text = "Update your info"
 end
 
 def update
+  # byebug
+
+  check = ((@user.username == params[:user][:username]) and (@user.email == params[:user][:email]))
+  if(check)
+    flash[:notice] = "No changes were made."
+    redirect_to user_path
+  else
+    if @user.update(get_user_params)
+      flash[:notice] = "#{@user.username.capitalize} updated successfully"
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
 end
 
 def show
